a5f91329c02979daf7b794f66ee9119da32e1b30
tluan1 <luanthanh253@gmail.com>
2024-06-17 09:15:26 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CSharp_Giaiptbac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbla = new System.Windows.Forms.Label();
+            this.lblb = new System.Windows.Forms.Label();
+            this.lblc = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.btngiaipt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblketqua = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(115, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ỨNG DỤNG GIẢI PHƯƠNG TRÌNH BẬC 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(286, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ax² + bx + c = 0";
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbla.Location = new System.Drawing.Point(102, 130);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(66, 19);
+            this.lbla.TabIndex = 2;
+            this.lbla.Text = "Nhập a";
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblb.Location = new System.Drawing.Point(102, 177);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(66, 19);
+            this.lblb.TabIndex = 3;
+            this.lblb.Text = "Nhập b";
+            // 
+            // lblc
+            // 
+            this.lblc.AutoSize = true;
+            this.lblc.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblc.Location = new System.Drawing.Point(102, 224);
+            this.lblc.Name = "lblc";
+            this.lblc.Size = new System.Drawing.Size(64, 19);
+            this.lblc.TabIndex = 4;
+            this.lblc.Text = "Nhập c";
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(182, 129);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.TabIndex = 5;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(182, 176);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 6;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(182, 223);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.TabIndex = 7;
+            // 
+            // btngiaipt
+            // 
+            this.btngiaipt.Location = new System.Drawing.Point(182, 260);
+            this.btngiaipt.Name = "btngiaipt";
+            this.btngiaipt.Size = new System.Drawing.Size(141, 43);
+            this.btngiaipt.TabIndex = 8;
+            this.btngiaipt.Text = "Giải phương trình";
+            this.btngiaipt.UseVisualStyleBackColor = true;
+            this.btngiaipt.Click += new System.EventHandler(this.btngiaipt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.groupBox1.Controls.Add(this.lblketqua);
+            this.groupBox1.Location = new System.Drawing.Point(106, 320);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 120);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblketqua
+            // 
+            this.lblketqua.AutoSize = true;
+            this.lblketqua.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblketqua.Location = new System.Drawing.Point(1, 48);
+            this.lblketqua.Name = "lblketqua";
+            this.lblketqua.Size = new System.Drawing.Size(59, 18);
+            this.lblketqua.TabIndex = 10;
+            this.lblketqua.Text = "Kết quả";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btngiaipt);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.lblc);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.lbla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.Label lblc;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button btngiaipt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblketqua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Giaiptbac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngiaipt_Click(object sender, EventArgs e)
+        {
+            double a, b, c, delta, x1, x2;
+            a = Convert.ToDouble(txta.Text);
+            b = Convert.ToDouble(txtb.Text);
+            c = Convert.ToDouble(txtc.Text);
+            if (a == 0)
+                if (b == 0)
+                    if (c != 0)
+                        lblketqua.Text = "phương trình vô nghiệm";
+                    else
+                        lblketqua.Text = "phương trình có vô số nghiệm";
+                else
+                {
+                    x1 = x2 = -c / b;
+                    lblketqua.Text = "phương trình có nghiệm x =" + x1;
+                }
+            else
+            {
+                delta = b * b - 4 * a * c;
+                if (delta < 0)
+                    lblketqua.Text = "phương trình vô nghiệm";
+                else
+                {
+                    x1 = x2 = -b / (2 * a);
+                    if (delta == 0)
+                        lblketqua.Text = "phương trình có nghiệm kép x1 = x2 =" + x1;
+
+
+
+                    else
+                    {
+                        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                        x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                        lblketqua.Text = "Phương trình có 2 nghiệm phân biệt:" + "\nx1 = " + x1 + "\nx2 = " + x2;
+                    }
+                }
+            }
+        }
+    }
+}
+
